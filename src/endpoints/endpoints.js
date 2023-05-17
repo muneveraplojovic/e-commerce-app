9f2a943f3a2695e7ee12c0dd0b3a64b538413419
Munevera Plojovic <plojovicm3@gmail.com>
2023-05-17 19:50:54 +0200

    login page and fetch func

---
+++ src/endpoints/endpoints.js
@@ -0,0 +1,13 @@
+export const userEndpoint={
+    fetchAll:"https://jsonplaceholder.typicode.com/users"
+}
+
+/*
+"www.nassajt.com/api/user/{:userId}"
+
+get - fetching data
+post - sending data - tek napravljeni
+put - update data - treba da ih merge-uje, merge:true, 
+delete - delete data/ user
+
+*/
