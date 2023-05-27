6982203694dd57ea6b0bb6b3801b76fbf5242e9d
Munevera Plojovic <plojovicm3@gmail.com>
2023-05-28 01:15:52 +0200

    links added, colors changed...

---
+++ src/Templates/Templates/DropdownDiv/DropdownDiv.jsx
@@ -0,0 +1,11 @@
+import { React, useState, useEffect } from "react";
+
+const styles = {
+  screen: { width: "100%", height: "50vh", margin: "50px 150px" },
+};
+
+const DropdownDiv = () => {
+  return <div style={styles.screen}></div>;
+};
+
+export default DropdownDiv;
